efecb8b4058df933b7304c6e268dd1b2cda95555
immithun <124258622+immithun@users.noreply.github.com>
2024-04-08 09:26:10 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,74 @@
+
+function scrollHeader() {
+  const header = document.getElementById("header");
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalClose = document.querySelectorAll(".services__modal-close");
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((mb, i) => {
+  mb.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalClose.forEach((mc) => {
+  mc.addEventListener("click", () => {
+    modalViews.forEach((mv) => {
+      mv.classList.remove("active-modal");
+    });
+  });
+});
+
+let mixer = mixitup(".work__container", {
+  selectors: {
+    target: ".work__card",
+  },
+  animation: {
+    duration: 300,
+  },
+});
+
+const workLinks = document.querySelectorAll(".work__item");
+
+function activeWork(workLink) {
+  workLinks.forEach((wl) => {
+    wl.classList.remove("active-work");
+  });
+  workLink.classList.add("active-work");
+}
+
+workLinks.forEach((wl) => {
+  wl.addEventListener("click", () => {
+    activeWork(wl);
+  });
+});
+
+let swiperTestimonial = new Swiper(".testimonial__container", {
+  spaceBetween: 24,
+  loop: true,
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 48,
+    },
+  },
+});
